961174038c4a18e342698a574c2fe5fdab1afd7b
Rajarajan <rajarajan.abathsagayam@gmail.com>
2025-04-07 10:23:49 +0530

    feat: add checklist screen for pre-departure block with progress indicator

--- src/screens/ChecklistScreen/ChecklistScreen.style.ts
+++ src/screens/ChecklistScreen/ChecklistScreen.style.ts
@@ -16,6 +16,31 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "transparent",
   },
+  ListRow: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
+    paddingLeft: 20,
+    backgroundColor: "white",
+  },
+  contentPart: {
+    flex: 1,
+    flexGrow: 1,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  arrowPart: {
+    alignItems: "flex-end",
+    justifyContent: "center",
+  },
+  progressText: {
+    fontSize: 12,
+    fontFamily: Typography.fontFamily.RobotoBold,
+    color: Colors.black,
+    textAlign: "center",
+  },
 });
 
 export default styles;
--- src/screens/ChecklistScreen/ChecklistsScreen.tsx
+++ src/screens/ChecklistScreen/ChecklistsScreen.tsx
@@ -1,11 +1,11 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {
   TouchableOpacity,
   FlatList,
   View,
-  Alert,
   Modal,
   StyleSheet,
+  Text,
 } from "react-native";
 import { StackScreenProps } from "@react-navigation/stack";
 import { RootStackParamList } from "../../types/navigation";
@@ -18,7 +18,6 @@ import {
   useAppSelector,
 } from "../../hooks/stateManagementHooks";
 import IconAdd from "../../assets/icons/iconAdd.svg";
-import checklistData from "../../mocks/ChecklistCategory";
 import ChecklistItemRenderer from "../../components/ChecklistItemRenderer";
 import AddNewChecklistScreen from "../MyChecklists/AddNewChecklistScreen/AddNewChecklistScreen";
 import { BlurView } from "@react-native-community/blur";
@@ -26,6 +25,9 @@ import {
   removeCheckListCategory,
   selectMyCheckList,
 } from "../../store/mychecklistSlice";
+import { Colors } from "../../theme";
+import IconArrow from "../../assets/icons/iconArrow.svg";
+import { AnimatedCircularProgress } from "react-native-circular-progress";
 
 type Props = StackScreenProps<RootStackParamList, "Checklists">;
 
@@ -40,6 +42,12 @@ const ChecklistsScreen: React.FC<Props> = ({ navigation }) => {
   const handleDelete = (id: string) => {
     dispatch(removeCheckListCategory({ titleId: id }));
   };
+  const progress = 65; // percentage
+  const circularRef = useRef<AnimatedCircularProgress>(null);
+
+  useEffect(() => {
+    circularRef.current?.animate(progress, 1000); // 1000ms = 1 second
+  }, [progress]);
 
   return (
     <SafeAreaView style={styles.container}>
@@ -49,6 +57,36 @@ const ChecklistsScreen: React.FC<Props> = ({ navigation }) => {
         subTitle="List of all required documents for your upcoming assignment"
         style={{ marginTop: 38 }}
       />
+
+      <View style={{ height: 80, marginTop: 12 }}>
+        <View style={styles.ListRow}>
+          <View style={styles.contentPart}>
+            <AnimatedCircularProgress
+              ref={circularRef}
+              size={41}
+              width={5}
+              fill={progress}
+              tintColor={Colors.marlowBlue}
+              backgroundColor="transparent"
+              rotation={0}
+              lineCap="square"
+              duration={1000}
+              onAnimationComplete={() => {}}
+            >
+              {(fill: number) => {
+                return (
+                  <Text style={styles.progressText}>{Math.round(fill)}%</Text>
+                );
+              }}
+            </AnimatedCircularProgress>
+            <Text style={{ marginLeft: 12 }}>Review List </Text>
+          </View>
+          <View style={styles.arrowPart}>
+            <IconArrow width={15} height={15} fill={Colors.grey600} />
+          </View>
+        </View>
+      </View>
+
       <TitleAndSubtitleSection
         title="My Checklists"
         subTitle="Create your own personal checklist"
